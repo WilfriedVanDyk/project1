fa5eb8a7b180da67721df6322cb2bfd86ddb4617
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2020-07-19 15:51:39 +0200

    een newfile

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-   
-    <HelloWorld msg="Welcome to Your Vue.js projectBranch1"/>
+    <head>
+  <title>InvoerApplicatie</title>
+</head>
+    <HelloWorld msg="Welcome to Your Vue.js projectBranch1" />
+<TheNavigation></TheNavigation>
+    <div id="nav">
+      
+    </div>
+    <router-view />
   </div>
 </template>
 
+
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheNavigation from "./components/TheNavigation.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    TheNavigation
   }
-}
+};
 </script>
 
 <style>
@@ -25,4 +37,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/InvoerPagina">InvoerPagina</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  scoped>
+#nav {
+    display: flex;
+    justify-content: center;
+    padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+
+#nav a.activeLink{
+  color: #b94252;
+}
+</style>
